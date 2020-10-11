5e5e9ab9129f155d0905be71c34880c58794509b
Ignat Seliun <seliunignat@gmail.com>
2020-10-11 15:34:27 +0300

    ADDED: Realised full MainFrame source code (maded 4.3.a)

--- src/bsu/rfe/java/group10/lab2/SELIUN/varC18/MainFrame.java
+++ src/bsu/rfe/java/group10/lab2/SELIUN/varC18/MainFrame.java
@@ -3,6 +3,9 @@ package bsu.rfe.java.group10.lab2.SELIUN.varC18;
 import javax.swing.*;
 import java.awt.*;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MainFrame extends JFrame{
 
     private static final int WIDTH = 400;
@@ -10,6 +13,7 @@ public class MainFrame extends JFrame{
 
     private JTextField textFieldX; //Добавили текстовые поля для ввода значений переменных
     private JTextField textFieldY;
+    private JTextField textFieldZ;
 
     private JTextField textFieldResult; //Текстовое поле для вывода результата вычислений формулы
 
@@ -19,13 +23,137 @@ public class MainFrame extends JFrame{
 
     private int formulaId = 1;
 
+    public Double calculate1(Double x, Double y, Double z)
+    {
+        return ((Math.sin(Math.PI*y*y) + Math.log(y*y))/(Math.sin(Math.PI*z*z) + Math.sin(x) + Math.log(z*z) + x*x + Math.exp(Math.cos(z*x))));
+    }
+
+    public Double calculate2(Double x, Double y, Double z)
+    {
+        return ((Math.pow(Math.cos(Math.exp(y)) + Math.exp(y*y) + Math.sqrt(1/x), 1/4))/(Math.pow( Math.cos(Math.PI*z*z*z) + Math.log((1+z)*(1+z)), Math.sin(y))));
+    }
+
+    private void addRadioButton(String buttonName, final int formulaId)
+    {
+        JRadioButton button = new JRadioButton(buttonName);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainFrame.this.formulaId = formulaId;
+                //imagePane.updateUI(); //???
+            }
+        });
+        radioButtons.add(button);
+        hboxFormulaType.add(button);
+    }
+
     public MainFrame()
     {
         super("Вычисление формулы");
         setSize(WIDTH, HEIGHT);
-
         Toolkit kit = Toolkit.getDefaultToolkit();
-        setLocation((kit.getScreenSize().width - WIDTH)/2,(kit.getScreenSize().height - HEIGHT)/2);
+// Отцентрировать окно приложения на экране
+        setLocation((kit.getScreenSize().width - WIDTH)/2,
+                (kit.getScreenSize().height - HEIGHT)/2);
+        hboxFormulaType.add(Box.createHorizontalGlue());
+        addRadioButton("Формула 1", 1);
+        addRadioButton("Формула 2", 2);
+        radioButtons.setSelected(radioButtons.getElements().nextElement().getModel(), true);
+        hboxFormulaType.add(Box.createHorizontalGlue());
+        hboxFormulaType.setBorder(BorderFactory.createLineBorder(Color.YELLOW));
+// Создать область с полями ввода для X и Y
+        JLabel labelForX = new JLabel("X:");
+        textFieldX = new JTextField("0", 10);
+        textFieldX.setMaximumSize(textFieldX.getPreferredSize());
+        JLabel labelForY = new JLabel("Y:");
+        textFieldY = new JTextField("0", 10);
+        textFieldY.setMaximumSize(textFieldY.getPreferredSize());
+        JLabel labelForZ = new JLabel("Z:");
+        textFieldZ = new JTextField("0", 10);
+        textFieldZ.setMaximumSize(textFieldZ.getPreferredSize());
+
+        Box hboxVariables = Box.createHorizontalBox();
+        hboxVariables.setBorder(BorderFactory.createLineBorder(Color.RED));
+
+        hboxVariables.add(Box.createHorizontalGlue());
+        hboxVariables.add(labelForX);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldX);
+
+        hboxVariables.add(Box.createHorizontalStrut(40));
+        hboxVariables.add(labelForY);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldY);
+
+        hboxVariables.add(Box.createHorizontalStrut(40));
+        hboxVariables.add(labelForZ);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldZ);
+
+        hboxVariables.add(Box.createHorizontalGlue());
+// Создать область для вывода результата
+        JLabel labelForResult = new JLabel("Результат:");
+//labelResult = new JLabel("0");
+        textFieldResult = new JTextField("0", 15);
+        textFieldResult.setMaximumSize(textFieldResult.getPreferredSize());
+        textFieldResult.setEditable(false);
+
+        Box hboxResult = Box.createHorizontalBox();
+        hboxResult.add(Box.createHorizontalGlue());
+        hboxResult.add(labelForResult);
+        hboxResult.add(Box.createHorizontalStrut(10));
+        hboxResult.add(textFieldResult);
+        hboxResult.add(Box.createHorizontalGlue());
+        hboxResult.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+// Создать область для кнопок
+        JButton buttonCalc = new JButton("Вычислить");
+        buttonCalc.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Double x = Double.parseDouble(textFieldX.getText());
+                    Double y = Double.parseDouble(textFieldY.getText());
+                    Double z = Double.parseDouble(textFieldZ.getText());
+                    Double result;
+                    if (formulaId==1)
+                        result = calculate1(x, y, z);
+                    else
+                        result = calculate2(x, y, z);
+                    textFieldResult.setText(result.toString());
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(MainFrame.this,
+                            "Ошибка в формате записи числа с плавающей точкой", "Ошибочный формат числа",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+        JButton buttonReset = new JButton("Очистить поля");
+        buttonReset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                textFieldX.setText("0");
+                textFieldY.setText("0");
+                textFieldZ.setText("0");
+                textFieldResult.setText("0");
+            }
+        });
+
+        Box hboxButtons = Box.createHorizontalBox();
+        hboxButtons.add(Box.createHorizontalGlue());
+        hboxButtons.add(buttonCalc);
+        hboxButtons.add(Box.createHorizontalStrut(30));
+        hboxButtons.add(buttonReset);
+        hboxButtons.add(Box.createHorizontalGlue());
+        hboxButtons.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+
+// Связать области воедино в компоновке BoxLayout
+        Box contentBox = Box.createVerticalBox(); //Создаем отдельный коробочный компановщик, и в него закидываем наши состовляющие, созданные до этого
+        contentBox.add(Box.createVerticalGlue());
+        contentBox.add(hboxFormulaType);
+        contentBox.add(hboxVariables);
+        contentBox.add(hboxResult);
+        contentBox.add(hboxButtons);
+        contentBox.add(Box.createVerticalGlue());
+
+        getContentPane().add(contentBox, BorderLayout.CENTER); //Помещаем нашу коробку(со всеми коробками(со всеми состовляющими)) в наш Frame
     }
 
     public static void main(String[] args) {
